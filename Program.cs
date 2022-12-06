0c938826d8eaf23814746a461ffdb400fae29897
Huxlyx <mtengler@gmail.com>
2022-12-06 21:24:58 +0100

    day 6 (quick & dirty)

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@ using System.Diagnostics;
 
 Stopwatch sw = new();
 sw.Start();
-Day05.RunDay05_2();
+Day06.RunDay06_2();
 Console.WriteLine("Done after " + sw.Elapsed);
---
+++ Src/Day06.cs
@@ -0,0 +1,75 @@
+﻿
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022.Src
+{
+    public class Day06
+    {
+        private const string INPUT = "Res/Day06.txt";
+
+        private static Regex matchNumbers = new(@"(\d)+", RegexOptions.Compiled);
+
+        public static void RunDay06_1()
+        {
+            var lines = File.ReadAllLines(INPUT);
+            string data = lines[0]; /* one datastream */
+            char[] chars = data.ToCharArray();
+
+            for (int i = 3; i < chars.Length; i++)
+            {
+                HashSet<char> set = new()
+                {
+                    chars[i - 3],
+                    chars[i - 2],
+                    chars[i - 1],
+                    chars[i]
+                };
+
+                if (set.Count == 4)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void RunDay06_2()
+        {
+            var lines = File.ReadAllLines(INPUT);
+            string data = lines[0]; /* one datastream */
+            char[] chars = data.ToCharArray();
+
+            for (int i = 13; i < chars.Length; i++)
+            {
+                HashSet<char> set = new()
+                {
+                    chars[i - 13],
+                    chars[i - 12],
+                    chars[i - 11],
+                    chars[i - 10],
+                    chars[i - 9],
+                    chars[i - 8],
+                    chars[i - 7],
+                    chars[i - 6],
+                    chars[i - 5],
+                    chars[i - 4],
+                    chars[i - 3],
+                    chars[i - 2],
+                    chars[i - 1],
+                    chars[i]
+                };
+
+                if (set.Count == 14)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
